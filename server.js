f58d1622f0bfd031d49e205de017e830dfa78d9b
Jakub Viták <jvitak@kerio.com>
2014-06-29 15:01:42 +0200

    fixed #3 - support subsequent urls

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var hapi = require('hapi');
 var server = new hapi.Server('0.0.0.0', Number(process.env.PORT || process.argv[2] || 8000));
 
 server.route({
-	path: '/',
+	path: '/{p*}',
 	method: 'GET',
 	handler: function(request, reply) {
 		var
 			uri,
+			link,
 			regex = /^www\./,
 			host = request.info.host;
 
@@ -19,8 +20,9 @@ server.route({
 
 		if (host.match(regex)) {
 			uri.host = host.replace(regex, '');
-			console.log(request.headers);
-			reply.redirect(url.format(uri)).code(301);
+			link = url.format(uri);
+			console.log(link, request.headers);
+			reply.redirect(link).code(301);
 		} else {
 			reply('This is not what you are probably looking for.');
 		}
